b1ef9ff328dffeab856ad00a0c624354dc32f062
xiaoh <xiaoh@192.168.2.15>
2021-03-17 18:17:32 -0400

    renamed package name: GarbageCardGameD2 to GarbageCardGame

---
+++ src/GarbageCardGame/Card.java
@@ -0,0 +1,61 @@
+package GarbageCardGame;
+
+public abstract class Card {
+
+	private char suit;
+	private int value;
+	private SUIT mySuit;
+	private VALUE myValue;
+
+	public void Deck() {
+		// TODO - implement Card.Deck
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param newSuit
+	 * @param newValue
+	 */
+	public void Deck(char newSuit, int newValue) {
+		// TODO - implement Card.Deck
+		throw new UnsupportedOperationException();
+	}
+
+	public char getSuit() {
+		return this.suit;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public String getValueString() {
+		// TODO - implement Card.getValueString
+		throw new UnsupportedOperationException();
+	}
+
+	public String toString() {
+		// TODO - implement Card.toString
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param deck
+	 */
+	public boolean compareSuit(Card deck) {
+		// TODO - implement Card.compareSuit
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param deck
+	 */
+	public boolean compareValue(Card deck) {
+		// TODO - implement Card.compareValue
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/GarbageCardGame/Garbage.java
@@ -0,0 +1,19 @@
+package GarbageCardGame;
+
+public class Garbage {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public void main(int[] args) {
+		// TODO - implement Garbage.main
+		throw new UnsupportedOperationException();
+	}
+
+	public void initializeGame() {
+		// TODO - implement Garbage.initializeGame
+		throw new UnsupportedOperationException();
+	}
+
+}
