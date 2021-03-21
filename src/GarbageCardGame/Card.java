524b028ad4a6b80675d05c50cb6db37017212a5f
xiaoh <xiaoh@192.168.2.15>
2021-03-21 16:02:36 -0400

    updated card constructor and toString method

--- src/GarbageCardGame/Card.java
+++ src/GarbageCardGame/Card.java
@@ -7,60 +7,46 @@ package GarbageCardGame;
  * Created: March 12, 2021 
  * Last updated: March 21, 2021
  */
-public abstract class Card {
-//enum datafield: value
 
-    private enum value {
-        DEUCE,
-        THREE,
-        FOUR,
-        FIVE,
-        SIX,
-        SEVEN,
-        EIGHT,
-        NINE,
-        TEN,
-        JACK,
-        QUEEN,
-        KING,
-        ACE
-    }
-
-//enum datafield: suits
-    private enum suits {
-        CLUBS,
-        DIAMONDS,
-        HEARTS,
+public final class Card {
+    
+    //enum datafield: suits
+    private enum Suit {
+        CLUBS, 
+        DIAMONDS, 
+        HEARTS, 
         SPADES
     }
+    
+    //enum datafield: suits
+    private enum Value {
+        ACE,
+        DEUCE, 
+        THREE, 
+        FOUR, 
+        FIVE, 
+        SIX, 
+        SEVEN, 
+        EIGHT, 
+        NINE, 
+        TEN, 
+        JACK, 
+        QUEEN, 
+        KING
+    }
 
-//	public char getSuit() {
-//		return this.suit;
-//	}
-//
-//	public int getValue() {
-//		return this.value;
-//	}
-//
-//	public String getValueString() {
-//		// TODO - implement Card.getValueString
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public String toString() {
-//		// TODO - implement Card.toString
-//		throw new UnsupportedOperationException();
-//	}
-//
-//
-//	public boolean compareSuit(Card deck) {
-//		// TODO - implement Card.compareSuit
-//		throw new UnsupportedOperationException();
-//	}
-//
-//
-//	public boolean compareValue(Card deck) {
-//		// TODO - implement Card.compareValue
-//		throw new UnsupportedOperationException();
-//	}
+    final Value value;
+    final Suit suit;
+
+// Card Constructor
+    private Card(final Value value, final Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+// toString method
+    @Override
+    public String toString() {
+        return String.format("%s of %s", value, suit);
+    }
 }
