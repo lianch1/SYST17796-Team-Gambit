8399c40f76efffb0287c1eaa649c69f6d509a583
xiaoh <xiaoh@192.168.2.15>
2021-03-21 17:31:30 -0400

    updated Deck() & swapCards()

--- src/GarbageCardGame/Deck.java
+++ src/GarbageCardGame/Deck.java
@@ -1,4 +1,5 @@
 package GarbageCardGame;
+import java.util.*;
 
 /**
  * This class contains allows the creation and manipulation of the deck
@@ -10,18 +11,37 @@ package GarbageCardGame;
 
 public class Deck {
 
-	public Deck() {
+    //create a deck of cards
+    public void Deck() {
 
-	}
+        ArrayList<Card> Deck = new ArrayList<Card>();
 
-	public void shuffle() {
-		// TODO - implement Deck.shuffle
-		throw new UnsupportedOperationException();
-	}
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Value value : Card.Value.values()) {
+                Deck.add(new Card(value, suit));
+            }
+        }
+    }       
 
-	public void generalDeck() {
-		// TODO - implement Deck.generalDeck
-		throw new UnsupportedOperationException();
-	}
+    //method to swap card positions in the deck
+    public void swapCards(int index1, int index2, ArrayList<Card> Deck){
+        Card placeholder;
+        
+        placeholder = Deck.get(index1);
+        Deck.set(index1, Deck.get(index2));
+        Deck.set(index2, placeholder);                
+    }
+    
+
+        
+    //shuffle a deck of cards
+    public void shuffle() {
+        // TODO - implement Deck.shuffle
+        throw new UnsupportedOperationException();
+    }
+    
+
+
+
 
 }
