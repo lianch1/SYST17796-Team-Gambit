cc2e30cd869f2f5bbc867b61c94a2707933bfd69
Cheng Lian <77893163+lianch1@users.noreply.github.com>
2021-03-21 19:49:03 -0400

    Cheng lian1 (#6)
    * renamed package name: GarbageCardGameD2 to GarbageCardGame
    
    * updated enums for suits and value
    
    * added documentation
    
    * updated card constructor and toString method
    
    * updated getters
    
    * added getValueNumber method
    
    * added documentation
    
    * updated Deck() & swapCards()
    
    * updated shuffle()
    
    * added Override to toString
    
    * updated

--- src/GarbageCardGame/Card.java
+++ src/GarbageCardGame/Card.java
@@ -1,61 +1,114 @@
 package GarbageCardGame;
 
+
+/**
+ * This class contains the value and suits of a deck of cards
+ * 
+ * author: Cheng Lian 
+ * Created: March 12, 2021 
+ * Last updated: March 21, 2021
+ */
+
 public abstract class Card {
+    
+    //enum datafield: suits
+    public enum Suit {
+        CLUBS, 
+        DIAMONDS, 
+        HEARTS, 
+        SPADES;
+    }
+    
+    //enum datafield: suits
+    public enum Value {
+        ACE,
+        DEUCE, 
+        THREE, 
+        FOUR, 
+        FIVE, 
+        SIX, 
+        SEVEN, 
+        EIGHT, 
+        NINE, 
+        TEN, 
+        JACK, 
+        QUEEN, 
+        KING;
+    }
 
-	private char suit;
-	private int value;
-	private SUIT mySuit;
-	private VALUE myValue;
+    final Value value;
+    final Suit suit;
 
-	public void Deck() {
-		// TODO - implement Card.Deck
-		throw new UnsupportedOperationException();
-	}
+// Card Constructor
+    Card(final Value value, final Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
 
-	/**
-	 * 
-	 * @param newSuit
-	 * @param newValue
-	 */
-	public void Deck(char newSuit, int newValue) {
-		// TODO - implement Card.Deck
-		throw new UnsupportedOperationException();
-	}
+// toString method
+    @Override
+    public String toString() {
+        return String.format("%s of %s", value, suit);
+    }
+    
+// getters and setters
+    public Value getValue() {
+        return value;
+    }
 
-	public char getSuit() {
-		return this.suit;
-	}
-
-	public int getValue() {
-		return this.value;
-	}
-
-	public String getValueString() {
-		// TODO - implement Card.getValueString
-		throw new UnsupportedOperationException();
-	}
-
-	public String toString() {
-		// TODO - implement Card.toString
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param deck
-	 */
-	public boolean compareSuit(Card deck) {
-		// TODO - implement Card.compareSuit
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param deck
-	 */
-	public boolean compareValue(Card deck) {
-		// TODO - implement Card.compareValue
-		throw new UnsupportedOperationException();
-	}
-
+    public Suit getSuit() {
+        return suit;
+    }
+    
+    
+// method for retriving card value in integer type  
+    public int getValueNumber(){
+        
+        int valueNumber = 0;
+        
+        switch (this.value){
+            case ACE:
+                valueNumber = 1;
+                break;
+            case DEUCE:
+                valueNumber = 2;
+                break;
+            case THREE:
+                valueNumber = 3;
+                break;
+            case FOUR:
+                valueNumber = 4;
+                break;
+            case FIVE:
+                valueNumber = 5;
+                break;
+            case SIX:
+                valueNumber = 6;
+                break;
+            case SEVEN:
+                valueNumber = 7;
+                break;
+            case EIGHT:
+                valueNumber = 8;
+                break;
+            case NINE:
+                valueNumber = 9;
+                break;
+            case TEN:
+                valueNumber = 10;
+                break;
+            case JACK:
+                valueNumber = 11;
+                break;
+            case QUEEN:
+                valueNumber = 12;
+                break;
+            case KING:
+                valueNumber = 13;
+                break;
+        }
+        return valueNumber;
+    }
+       
+    
 }
--- src/GarbageCardGame/Deck.java
+++ src/GarbageCardGame/Deck.java
@@ -1,23 +1,44 @@
 package GarbageCardGame;
 
+import java.util.*;
+import java.util.Random;
+
+/**
+ * This class contains allows the creation and manipulation of the deck
+ * 
+ * author: Cheng Lian 
+ * Created: March 12, 2021 
+ * Last updated: March 21, 2021
+ */
+
 public class Deck {
+       
+    //create a deck of cards     
+    Deck(ArrayList<Card> Deck) {
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Value value : Card.Value.values()) {
+                Deck.add(new Card(value, suit));
+                
+            }
+        }
+    }    
 
-	private ArrayList<Card> deck;
-	private ArrayList<Card> discard;
-
-	public Deck() {
-		// TODO - implement Deck.Deck
-		throw new UnsupportedOperationException();
-	}
-
-	public void shuffle() {
-		// TODO - implement Deck.shuffle
-		throw new UnsupportedOperationException();
-	}
-
-	public void generalDeck() {
-		// TODO - implement Deck.generalDeck
-		throw new UnsupportedOperationException();
-	}
-
-}
+    //method to swap card positions in the deck
+    public void swapCard(int index1, int index2, ArrayList<Card> Deck){
+        Card placeholder;
+        
+        placeholder = Deck.get(index1);
+        Deck.set(index1, Deck.get(index2));
+        Deck.set(index2, placeholder);                
+    }
+            
+    //shuffle a deck of cards
+    public void shuffle(ArrayList<Card> Deck) {
+        Random randomNumber = new Random();
+        for (int i = 0; i < Deck.size(); i++){
+            for (int j = 0; j < Deck.size(); j++){
+                swapCard(i, randomNumber.nextInt(52), Deck);
+            }
+        }
+    }
+    
