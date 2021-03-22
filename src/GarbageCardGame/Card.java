f06b4ccc9304b95e16cdae2d4b4016947f4e4039
Cheng Lian <77893163+lianch1@users.noreply.github.com>
2021-03-22 16:48:02 -0400

    Cheng lian2 (#7)
    * renamed package name: GarbageCardGameD2 to GarbageCardGame
    
    * updated enums for suits and value
    
    * added documentation
    
    * updated card constructor and toString method
    
    * updated getters
    
    * added getValueNumber method
    
    * added documentation
    
    * updated Deck() & swapCards()
    
    * updated shuffle()
    
    * added Override to toString
    
    * updated
    
    * Moved enum Suits outside of Card Class
    
    * Moved enum Values outside of Card Class
    
    * Removed enum from card class and updated constructors and methods
    
    * updated constructor: Deck() and method: createDeck()
    
    * commit 4pm
    
    * Created new branch

--- src/GarbageCardGame/Card.java
+++ src/GarbageCardGame/Card.java
@@ -1,113 +1,54 @@
+
 package GarbageCardGame;
 
-
 /**
- * This class contains the value and suits of a deck of cards
+ * This class allows the creation of a card as well as ways to manipulate it
  * 
  * author: Cheng Lian 
  * Created: March 12, 2021 
- * Last updated: March 21, 2021
+ * Last updated: March 22, 2021
  */
 
-public abstract class Card {
+public class Card {
     
-    //enum datafield: suits
-    public enum Suit {
-        CLUBS, 
-        DIAMONDS, 
-        HEARTS, 
-        SPADES;
-    }
-    
-    //enum datafield: suits
-    public enum Value {
-        ACE,
-        DEUCE, 
-        THREE, 
-        FOUR, 
-        FIVE, 
-        SIX, 
-        SEVEN, 
-        EIGHT, 
-        NINE, 
-        TEN, 
-        JACK, 
-        QUEEN, 
-        KING;
-    }
-
-    final Value value;
-    final Suit suit;
+    private Value value;
+    private Suit suit;
+    private boolean isFaceUp;
 
 // Card Constructor
-    Card(final Value value, final Suit suit) {
+    public Card(Value value, Suit suit) {
         this.value = value;
         this.suit = suit;
-    }
-
-// toString method
-    @Override
-    public String toString() {
-        return String.format("%s of %s", value, suit);
+        isFaceUp = true;
     }
     
 // getters and setters
-    public Value getValue() {
-        return value;
+    public int getValueNumber() {
+        return value.getValueNumber();
+    }
+    
+    public String getValueString(){
+        return value.getValueString();
     }
 
-    public Suit getSuit() {
-        return suit;
+    public String getSuit() {
+        return suit.printSuit();
+    }
+//toString method
+    public String toString() {
+        String txt = "";
+        if (isFaceUp){
+            txt += "\n" + value.getValueString() + " of " + suit.printSuit();
+            return txt;
+        } else {
+            txt = "\nCard is Faced down ";
+        }
+        return txt;
     }
     
-    
-// method for retriving card value in integer type  
-    public int getValueNumber(){
-        
-        int valueNumber = 0;
-        
-        switch (this.value){
-            case ACE:
-                valueNumber = 1;
-                break;
-            case DEUCE:
-                valueNumber = 2;
-                break;
-            case THREE:
-                valueNumber = 3;
-                break;
-            case FOUR:
-                valueNumber = 4;
-                break;
-            case FIVE:
-                valueNumber = 5;
-                break;
-            case SIX:
-                valueNumber = 6;
-                break;
-            case SEVEN:
-                valueNumber = 7;
-                break;
-            case EIGHT:
-                valueNumber = 8;
-                break;
-            case NINE:
-                valueNumber = 9;
-                break;
-            case TEN:
-                valueNumber = 10;
-                break;
-            case JACK:
-                valueNumber = 11;
-                break;
-            case QUEEN:
-                valueNumber = 12;
-                break;
-            case KING:
-                valueNumber = 13;
-                break;
-        }
-        return valueNumber;
+//flip card method    
+    public void flipCard(){
+        isFaceUp = !isFaceUp;
     }
        
     
