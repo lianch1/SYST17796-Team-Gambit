e0e0e071e5911316a876e86d8f090ea72d4ab724
xiaoh <xiaoh@192.168.2.15>
2021-03-22 16:03:20 -0400

    Removed enum from card class and updated constructors and methods

--- src/GarbageCardGame/Card.java
+++ src/GarbageCardGame/Card.java
@@ -1,112 +1,54 @@
 package GarbageCardGame;
 
 /**
- * This class contains the value and suits of a deck of cards
+ * This class allows the creation of a card as well as ways to manipulate it
  * 
  * author: Cheng Lian 
  * Created: March 12, 2021 
- * Last updated: March 21, 2021
+ * Last updated: March 22, 2021
  */
 
 public final class Card {
     
-    //enum datafield: suits
-    public enum Suit {
-        CLUBS, 
-        DIAMONDS, 
-        HEARTS, 
-        SPADES;
-    }
-    
-    //enum datafield: suits
-    public enum Value {
-        ACE,
-        DEUCE, 
-        THREE, 
-        FOUR, 
-        FIVE, 
-        SIX, 
-        SEVEN, 
-        EIGHT, 
-        NINE, 
-        TEN, 
-        JACK, 
-        QUEEN, 
-        KING;
-    }
-
-    final Value value;
-    final Suit suit;
+    private Values values;
+    private Suits suits;
+    private boolean isFaceUp;
 
 // Card Constructor
-    Card(final Value value, final Suit suit) {
-        this.value = value;
-        this.suit = suit;
-    }
-
-// toString method
-    @Override
-    public String toString() {
-        return String.format("%s of %s", value, suit);
+    public Card(Values values, Suits suits) {
+        this.values = values;
+        this.suits = suits;
+        isFaceUp = false;
     }
     
 // getters and setters
-    public Value getValue() {
-        return value;
+    public int getValueNumber() {
+        return values.getValueNumber();
+    }
+    
+    public String getValueString(){
+        return values.getValueString();
     }
 
-    public Suit getSuit() {
-        return suit;
+    public String getSuit() {
+        return suits.printSuit();
+    }
+//toString method
+
+    public String toString() {
+        String txt = "";
+        if (isFaceUp) {
+            txt += "\n" + values.getValueString() + " of " + suits.printSuit();
+            return txt;
+        } else {
+            txt = "Card is Faced down";
+        }
+        return txt;
     }
     
-    
-// method for retriving card value in integer type  
-    public int getValueNumber(){
-        
-        int valueNumber = 0;
-        
-        switch (this.value){
-            case ACE:
-                valueNumber = 1;
-                break;
-            case DEUCE:
-                valueNumber = 2;
-                break;
-            case THREE:
-                valueNumber = 3;
-                break;
-            case FOUR:
-                valueNumber = 4;
-                break;
-            case FIVE:
-                valueNumber = 5;
-                break;
-            case SIX:
-                valueNumber = 6;
-                break;
-            case SEVEN:
-                valueNumber = 7;
-                break;
-            case EIGHT:
-                valueNumber = 8;
-                break;
-            case NINE:
-                valueNumber = 9;
-                break;
-            case TEN:
-                valueNumber = 10;
-                break;
-            case JACK:
-                valueNumber = 11;
-                break;
-            case QUEEN:
-                valueNumber = 12;
-                break;
-            case KING:
-                valueNumber = 13;
-                break;
-        }
-        return valueNumber;
+//flip card method    
+    public void flipCard(){
+        isFaceUp = !isFaceUp;
     }
        
     
