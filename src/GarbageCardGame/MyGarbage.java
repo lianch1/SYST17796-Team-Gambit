14dc3e860244a4145a39d566c59d66dc232a1836
xiaoh <xiaoh@192.168.2.144>
2021-04-11 16:32:43 -0400

    updated myGarbage.java with playerAgain sequence

--- src/GarbageCardGame/MyGarbage.java
+++ src/GarbageCardGame/MyGarbage.java
@@ -18,46 +18,62 @@ public class MyGarbage {
         String player1Name = "";
         String player2Name = "";
         
-        //variable to keep track of the playerCount, 2 players needed to start the game       
-        int playerCount = 0;
 
-        while (playerCount < 2) {
-            for (int i = 1; i < 3; i++) {
-                boolean isNameValid = false;
-                String playerName = "";
-                while (isNameValid == false) {
-                    System.out.println("Please enter a valid name for player " + i + "(Letters and numbers only): ");
-                    playerName = input.nextLine();
-                    if (isValid(playerName) == true){
-                        isNameValid = true;
-                    }
+        //method for prompts the user to enter their names and checks if it's valid
+        for (int i = 1; i < 3; i++) {
+            boolean isNameValid = false;
+            String playerName = "";
+            while (isNameValid == false) {
+                System.out.println("Please enter a valid name for player " + i + "(Letters and numbers only): ");
+                playerName = input.nextLine();
+                if (isValid(playerName) == true) {
+                    isNameValid = true;
                 }
-                Player playerObject = new Player(playerName);
-                if (playerObject.findPlayerIndex(playerName) == -1) {
-                    //player doesn't exist            
-                    playerObject.addPlayer(playerObject);
-                } else {
-                    //player exists            
-                    System.out.println("Welcome back " + playerName);
-                }
-                if (i == 1) {
-                    player1Name = playerObject.getPlayerID();
-                } else if (i == 2) {
-                    player2Name = playerObject.getPlayerID();
-                }
-                playerCount += 1;
-
+            }
+            if (i == 1) {
+                player1Name = playerName;
+            }
+            if (i == 2) {
+                player2Name = playerName;
             }
         }
-    
 
 
+        
+        //testing to see if driver program will take existing object from array
+        //Player alex = new Player("Alex", 100);
+        //alex.addPlayer();
+        
+        //initialize the both player objects
+        Player playerObject1 = new Player(player1Name, 0);
+        Player playerObject2 = new Player(player2Name, 0);
+
+        //return player index if they exists already
+        int player1Index = playerObject1.getPlayerIndex(player1Name);
+        int player2Index = playerObject2.getPlayerIndex(player2Name);
+        
+        //checking if player already exists
+        if (player1Index == -1) {
+            playerObject1.addPlayer();
+        } else {
+            playerObject1 = playerObject1.getPlayer(player1Index);
+        }
+
+        if (player2Index == -1) {
+            playerObject2.addPlayer();
+        } else {
+            playerObject2 = playerObject2.getPlayer(player2Index);
+        }
+        
+        boolean playAgain = true;
+        while(playAgain == true){
+        
         //create new deck and shuffle it
         Deck d1 = new Deck();
         d1.createDeck();
         d1.shuffle();
 
-        //create player objects and deal 10 faced down cards each to them
+        //create playerCards objects and deal 10 faced down cards each to them
         PlayerCards player1 = new PlayerCards(d1);
         PlayerCards player2 = new PlayerCards(d1);
 
@@ -71,20 +87,45 @@ public class MyGarbage {
             player2Won = player2.turn(player2, d1, player2Name);
         }
 
-        //winner declaration
+
+                
+        //winner declaration and updates score
         final String PURPLE = "\033[0;35m";
         final String RESET = "\033[0m";
         if (player1Won == true && player2Won == true) {
-            System.out.println(PURPLE + "***************** " +"Both " + player1Name + " and " + player2Name + " Won!!!!" + "*****************" + RESET);
+            System.out.println(PURPLE + "***************** " +"Both " + player1Name + " and " + player2Name + " Won!!!!" + "*****************" + RESET);          
+            playerObject1.setScore(playerObject1.getScore() + 1);
+            playerObject2.setScore(playerObject2.getScore() + 1);
         } else if (player1Won == true) {
             System.out.println(PURPLE + "***************** " +player1Name + " Won!!!!"+ "*****************" + RESET);
+            playerObject1.setScore(playerObject1.getScore() + 1);
         } else if (player2Won == true) {
-            System.out.println(PURPLE + "***************** " + player1Name + " 2 Won!!!!"+ "*****************" + RESET);
+            System.out.println(PURPLE + "***************** " + player2Name + " Won!!!!"+ "*****************" + RESET);
+            playerObject2.setScore(playerObject2.getScore() + 1);
         }
-       
-
-
         
+
+            //print player scores:
+            System.out.println(player1Name + "'s total score is " + playerObject1.getScore());
+            System.out.println(player2Name + "'s total score is " + playerObject2.getScore());
+
+
+            //play again?
+            String newGame = "";
+            while (!newGame.equalsIgnoreCase("yes")  && !newGame.equalsIgnoreCase("no")) {
+                System.out.println("Would you like to play again? (yes/no)");
+                newGame = input.nextLine();
+                if (newGame.equalsIgnoreCase("no")) {
+                    playAgain = false;
+                } else if (newGame.equalsIgnoreCase("yes")) {
+                    playAgain = true;
+                } else {
+                    System.out.println("Please enter yes/no!");
+                }
+                
+            }
+            
+        }
     }
     //method for checking if the name entered is valid
     public boolean isValid(String playerID) {
@@ -98,6 +139,9 @@ public class MyGarbage {
         }
         return true;
     }
+    
+
+
     
     public static void main(String[] args) {
         MyGarbage self = new MyGarbage();
