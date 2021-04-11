4bf445f6cfbe937ea2872f36c95e2f5db93787ed
xiaoh <xiaoh@192.168.2.144>
2021-04-11 17:32:56 -0400

    added comments added author: Fei Wei

--- src/GarbageCardGame/Suit.java
+++ src/GarbageCardGame/Suit.java
@@ -1,24 +1,36 @@
 package GarbageCardGame;
 
 /**
- * 
+ An enum class that models the suits for playing card Objects. 
+ * It has a suit (clubs, hearts, spades, diamonds).
+ *
  * @author Cheng Lian April 10, 2021
+ * @author Fei Wei April 11, 2021
  */
 public enum Suit {
+    
         //enum datafield: suits
         CLUBS("Clubs"), 
         DIAMONDS("Diamonds"), 
         HEARTS("Hearts"), 
         SPADES("Spades");
         
+        /**
+         * This field variable represent a string suit
+         */
         private final String suitString;
         
-        // Constructor
+        /**
+         * Default constructor
+         * @param suitString 
+         */
         private Suit(String suitString){
             this.suitString = suitString;
     }
-        
-        //Public Method
+           
+        /**
+         * @return the suit
+         */
         public String printSuit(){
             return suitString;
         }
